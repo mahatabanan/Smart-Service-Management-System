0c1458dda53a8a52edff82c55e3b2e5a95b2d2f5
antuvai <tanvirmahatabanan@gmail.com>
2026-01-13 16:48:53 +0600

    ad js validation update profile

--- View/js/userupdatevalidation.js
+++ View/js/userupdatevalidation.js
@@ -2,6 +2,7 @@ function validateuserform() {
     // ইনপুট ফিল্ড সিলেক্ট করা
     const nameInput = document.getElementById("uname");
     const name = nameInput.value.trim();
+    const password = document.getElementById("pass").value;
 
     // Validation Rules
     if (name === "") {
@@ -9,8 +10,17 @@ function validateuserform() {
         return false; // ফর্ম সাবমিট আটকাবে
     }
 
-    if (name.length < 3) {
-        alert("Name must be at least 3 characters");
+     if (password == "") {
+        alert("Password cannot be empty");
+        return false; // ফর্ম সাবমিট আটকাবে
+    }
+
+    if (password.length < 4) {
+        alert("Password must be at least 3 characters");
+        return false; // ফর্ম সাবমিট আটকাবে
+    }
+    if (name.length < 4) {
+        alert("Name must be at least 4 characters");
         return false; // ফর্ম সাবমিট আটকাবে
     }
 
--- View/updateuser.php
+++ View/updateuser.php
@@ -3,17 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Update Profile</title>
-    <style>
-        body { margin: 0; font-family: Arial, sans-serif; background-color: #eef3f4; }
-        .top-header { background-color: #006680; padding: 20px 40px; color: white; font-size: 22px; font-weight: bold; }
-        .profile-container { width: 60%; margin: 60px auto; background: white; padding: 40px; border-radius: 10px; box-shadow: 0 6px 18px rgba(0,0,0,0.1); }
-        .profile-container h1 { font-size: 20px; margin-bottom: 15px; color: #003d4d; }
-        .profile-container span { color: #444; font-weight: normal; }
-        .divider { margin: 25px 0; border-bottom: 1px solid #ddd; }
-        input[type="text"] { width: 70%; padding: 10px; border: 1px solid #ccc; border-radius: 6px; outline: none; }
-        input[type="submit"] { padding: 10px 25px; margin-left: 10px; background-color: #006680; color: white; border: none; border-radius: 6px; cursor: pointer; }
-        input[type="submit"]:hover { background-color: #004d61; }
-    </style>
+    <link rel="stylesheet" href="../View/css/updatestyle.css">
 </head>
 <body>
 
@@ -26,7 +16,7 @@
     <div class="divider"></div>
 
     <h3>Edit Information:</h3>
-    <form method="POST" action="../Controller/userupdatecontroller.php" required>
+    <form method="POST" action="../Controller/userupdatecontroller.php"onsubmit="return validateuserform(); " required>
         <input type="text"  id="uname" name="uname"  placeholder="Enter new name">
         <br><br>
         <input type="text"  id="pass" name="pass"  placeholder="Enter new Password"><br><br>
@@ -34,6 +24,6 @@
     </form>
 
 </div>
-
+<script src="../View/js/userupdatevalidation.js"></script>
 </body>
 </html>
