5fee94865fa0fe7f3fd0b9ed5d4861c490635d4e
antuvai <tanvirmahatabanan@gmail.com>
2026-01-15 20:53:18 +0600

    signup backend updated
    npw customer is fully updated

--- View/Customer/signup.php
+++ View/Customer/signup.php
@@ -1,9 +1,9 @@
 <?php
 // database connection
-require "db.php";
+//require "..db.php";
 
 // form submit হলে এই অংশ কাজ করবে
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name     = $_POST['name'];
     $password = $_POST['password'];
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $address  = $_POST['address'];
 
     // basic server-side validation
-    if ($name != "" && $password != "" && $mobile != "" && $address != "") {
+   /* if ($name != "" && $password != "" && $mobile != "" && $address != "") {
 
         // INSERT using correct DB column name: mobile
         $sql = "INSERT INTO users (name, password, mobile, address)
@@ -31,21 +31,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $error = "All fields are required!";
     }
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Registration - Smart Service System</title>
-    <link rel="stylesheet" href="signup.css">
+    <link rel="stylesheet" href="../../View/css/signup.css">
 </head>
 <body>
 
 <div id="card">
 
     <div id="section">
-        <img src="logo.png" id="logo" alt="Logo">
+        <img src="../../View/logo/logo.png" id="logo" alt="Logo">
     </div>
 
     <h3>Registration Form</h3>
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
 
     <!-- same file submit -->
-    <form method="post" onsubmit="return validateForm();">
+    <form  action="../../Controller/signupcontroller.php" method="POST" onsubmit="return validateForm();">
 
         <label for="name">Name</label>
         <input type="text" id="name" name="name" placeholder="Enter your full name">
@@ -78,6 +78,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </div>
 
-<script src="signup.js"></script>
+<script src="../../View/js/signup.js"></script>
 </body>
 </html>
